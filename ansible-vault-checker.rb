4f153f3c1a63e16e9025d7a3b20fd94f9a670723
Ridho Adya Pangestu <ridhoadya@gmail.com>
2019-10-18 08:01:02 +0700

    Support hash and array vault

--- ansible-vault-checker.rb
+++ ansible-vault-checker.rb
@@ -7,11 +7,27 @@ abort "Usage: ansible-vault-checker PASSWORDPATH 'FILEPATH'" if ARGV.length != 2
 
 passwordpath = ARGV[0]
 filepath = ARGV[1]
+vault_data = {}
 
 Dir.glob(filepath).each do |filename|
   data = YAML.load(File.read(filename))
   puts "Checking vault in #{filename} file"
-  vault_data = data.select { |key, value| value.to_s.include? "VAULT" }
+  data.each do |key, value|
+    if value.is_a?(Hash)
+      value.each do |k,v|
+        next if !v.to_s.include? "VAULT"
+        vault_data["#{key}.#{k}"] = v
+      end
+    elsif value.is_a?(Array)
+      value.each_with_index do |v, i|
+        next if !v.to_s.include? "VAULT"
+        vault_data["#{key}.#{i}"] = v
+      end
+    else
+      next if !value.to_s.include? "VAULT"
+      vault_data[key] = value
+    end
+  end
   vault_data.each do |key, value|
     if !value.start_with? "$ANSIBLE_VAULT;1.1;AES256"
       puts "  - #{key} => value must start with $ANSIBLE_VAULT;1.1;AES256"
